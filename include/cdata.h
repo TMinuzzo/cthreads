a5103ed063fbc2e86724771f277ebce44cc5149e
tminuzzo <thaynaminuzzo@gmail.com>
2019-10-05 13:06:10 -0300

    Added guidelines in lib.c

--- include/cdata.h
+++ include/cdata.h
@@ -1,7 +1,8 @@
 /*
- * cdata.h: arquivo de inclus�o de uso apenas na gera��o da libpithread
+
+ * cdata.h: arquivo de inclusão de uso apenas na geração da libpithread
  *
- * VERS�O: 29/03/2019
+ * VERSÃO: 29/03/2019
  *
  */
 #ifndef __cdata__
@@ -10,21 +11,23 @@
 
 /* Estados das threads
 */
-#define	PROCST_APTO	0		/* Processo em estado apto a ser escalonado */
-#define	PROCST_EXEC	1		/* Processo em estado de execu��o */
-#define	PROCST_BLOQ	2		/* Processo em estado bloqueado */
-#define	PROCST_TERMINO	3	/* Processo em estado de terminado */
 
+#define PROCST_NOVO 0		/* Processo criado */
+#define	PROCST_APTO	1		/* Processo em estado apto a ser escalonado */
+#define	PROCST_EXEC	2		/* Processo em estado de execu��o */
+#define	PROCST_BLOQ	3		/* Processo em estado bloqueado */
+#define	PROCST_TERMINO	4	/* Processo em estado de terminado */
 
-typedef struct s_TCB { 
-	/* OS CAMPOS ABAIXO N�O PODEM SER ALTERADOS OU REMOVIDOS
+ 
+typedef struct s_TCB { //thread control block
+	/* OS CAMPOS ABAIXO NÃO PODEM SER ALTERADOS OU REMOVIDOS
 	*/
 	int		tid; 		// identificador da thread
 	int		state;		// estado em que a thread se encontra (CONFORME DEFINES ANTERIORES)
 	int 		prio;		// Prioridade associada a thread
-	ucontext_t 	context;	// contexto de execu��o da thread (SP, PC, GPRs e recursos) 
+	ucontext_t 	context;	// contexto de execução da thread (SP, PC, GPRs e recursos) 
 	
-	/* A PARTIR DESSE PONTO PODEM SER ACRESCENTADOS CAMPOS NECESS�RIOS AO DESENVOLVIMENTO DA BIBLIOTECA
+	/* A PARTIR DESSE PONTO PODEM SER ACRESCENTADOS CAMPOS NECESSÁRIOS AO DESENVOLVIMENTO DA BIBLIOTECA
 	*/
 	
 } TCB_t; 
--- src/lib.c
+++ src/lib.c
@@ -9,34 +9,119 @@
 	OBS: Todas as funções devem testar no começo se a chamada
 	vem da main() - vide Seção 4 da definição do trabalho
 */
+int tid = 0;
 
-
-int ccreate (void* (*start)(void*), void *arg, int prio) {
-	return -9;
+/*
+ * Creates a new thread
+ * Parametes:
+ * 	start: pointer to function the thread will run
+ *  arg: general parameter
+ *  prio: priority, should NOT be used, will always be zero
+ * 
+ * Return:
+ *	 0: Success
+ *	-1: Error  
+*/
+int ccreate (void* (*start)(void*), void *arg, int prio) 
+{
+	// TODO: create context to pass int he TCB_t struct
+	TCB_t *newThread = malloc(sizeof(TCB_t));
+	 *newThread = (TCB_t) { 
+		 .tid = tid,
+        .state = PROCST_APTO,
+        .prio = prio,
+        //.context = context
+	 };
+	tid++;
+	// TODO: insert newThread in 'fila de aptos'
+	return -1;
 }
 
+/*
+ * Voluntary transfer
+ * 
+ * Return:
+ *	 0: Success
+ *	-1: Error  
+*/
 int cyield(void) {
+	// TODO: idenfity which thread is running (EXECUTANDO) and release it, sending to APTO state
+	// TODO: call scheduler to choose the new thread to go to EXECUTANDO
 	return -9;
 }
 
+/*
+ * Parameters:
+ * 	tid: thread identifier 
+ * Return:
+ *	 0: Success
+ *	-1: Error  
+*/
 int cjoin(int tid) {
+	// TODO: identify which thread is running (EXECUTANDO) and send it BLOQUEADO
+	// TODO: use the tid to identify which thread should wait to finish execution
+	// should return when identified that the thread finished
 	return -9;
+
 }
 
+/* Initialize the csem_t structure, that represents a semaphore
+ * Parameters:
+ * 	csem_t: pointer to semaphore
+ *  count: value to initialize semaphore. Represents how many resources the semaphore controls
+ * 		   for a binary semaphore, count = 1
+ * 
+ * Return:
+ *	 0: Success
+ *	-1: Error  
+*/
 int csem_init(csem_t *sem, int count) {
-	return -9;
+	// TODO: initialize csem_t before using it's value in cwait and csignal
+	// TODO: csem_t receives as a second parameter a queue with all the blocked threads
+	return -9;	
 }
 
+/* 
+ * Request a resource protected by semaphore, and manage a queue if the resource is being used
+ * Parameters:
+ * 	csem_t: pointer to semaphore
+ * 
+ * Return:
+ *	 0: Success
+ *	-1: Error  
+*/
 int cwait(csem_t *sem) {
+	// TODO: verify if the requested resource is free.
+	// 		 YES: assigns the resource to the running thread
+	//		 NO: blocks the running thread, and send it to a queue that waits for the resource (semaphore queue)
+	// TODO: decrement the csem_t count variable: (sem->count)--				
 	return -9;
 }
 
+/*
+ * Release a resource protected by semaphore
+ * Parameters:
+ * 	csem_t: pointer to semaphore
+ * Return:
+ *	 0: Success
+ *	-1: Error  
+*/
 int csignal(csem_t *sem) {
+	// TODO: 
+	// TODO: increment the csem_t count variable: (sem->count)++
 	return -9;
 }
 
+/*
+ * 
+ * Parameters:
+ * 	name: pointer to memory area wich contains the names and card ids
+ * Return:
+ *	 0: Success
+ *	-1: Error  
+*/
 int cidentify (char *name, int size) {
-	strncpy (name, "Gabriel Lando - 00291399\nLeonardo Lauryel - XXXXXXXX\nThayná Minuzzo - XXXXXXXX", size);
+	strncpy (name, "Gabriel Lando - 00291399\nLeonardo Lauryel - XXXXXXXX\nThayná Minuzzo - 00262525", size);
 	return 0;
 }
 
